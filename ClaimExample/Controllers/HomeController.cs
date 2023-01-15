d298211df7ebf5a55d2ab1cb48ba0aea3563ae7d
Minh Tam <minhtam.seta@gmail.com>
2023-01-16 06:43:51 +0700

    Initial a project

---
+++ ClaimExample/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using ClaimExample.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ClaimExample.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[Authorize]
+		public IActionResult Secret()
+		{
+			return View();
+		}
+
+		public IActionResult Authenticate()
+		{
+			return RedirectToAction("Index");
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ ClaimExample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClaimExample.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
